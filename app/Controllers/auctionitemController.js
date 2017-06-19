eff1e9422e30dd31c461b8edb4da3d60dd35f461
nhphatnhphat@gmail.com <nhphatnhphat@gmail.com>
2017-06-19 23:06:09 +0700

    user now can bid, comment, fix countdown time realtime,  lock item when time's up, user jsut can bid when they already login, if not -> login , send mail to make sure that user really want to bid, check user can bid when pos/(pos+nega) > 0.8, check user bid the price must follow the step

--- app/Controllers/auctionitemController.js
+++ app/Controllers/auctionitemController.js
@@ -1,4 +1,5 @@
 var auctionitemdb = require("../models/auction_item.js");
+var momment=require("moment");
 var Qs = require('q');
 
 var auctionitemController = {
@@ -7,21 +8,90 @@ var auctionitemController = {
     Qs.all([auctionitemdb.loadWithID(proId), auctionitemdb.loadSellerInfo(proId), auctionitemdb.loadHighestBuyerInfo(proId), auctionitemdb.loadTotalItemSeller(proId),
                     auctionitemdb.loadTotalPersonBid(proId), auctionitemdb.loadBidHistory(proId), auctionitemdb.loadComment(proId),auctionitemdb.getMaxBidAndStep(proId)
                     , auctionitemdb.getCatogory()])
-                    .spread(function (temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9) {
+                    .spread(function (item, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9) {
+      var user = req.session.user;
+      var moment_now = momment(new Date);
+      var moment_post = momment(item.datepost);
+      var moment_finish = momment(item.datefinish);
+      var isTimeNotAvailable = !(moment_post.isBefore(moment_now) && moment_now.isBefore(moment_finish));
+      var isRatingNotAvailable = false;
+      if (user){
+          isRatingNotAvailable = user.Positiverating / (user.Negativerating + user.Positiverating) < 0.8;
+      }
       res.render("_productAuction/item", {
-        user : req.session.user,
+        user : user,
         catogorylist : temp9,
         layout : "application",
-        item : temp1,
+        item : item,
         seller : temp2,
         highestbuyerid : temp3,
         sellertotalitems : temp4,
         totalPersonBid : temp5,
         bidhistory : temp6,
         comment : temp7,
-        maxbidandstep : temp8
+        maxbidandstep : temp8,
+        isTimeNotAvailable: isTimeNotAvailable,
+        isRatingNotAvailable: isRatingNotAvailable
       });
     });
-  }
-}
+  },
+    addComment: function (req, res) {
+        var idItem = req.params.id;
+        var idUser = req.session.user.IdUser;
+        var datepost=momment().format('YYYY/MM/DD H:mm:ss');
+        auctionitemdb.addComment(idUser, idItem, req.body.content,datepost).then(function () {
+            return res.redirect('back');
+        }).fail(function (err) {
+            console.log(err);
+            res.end('fail');
+        });
+    },
+    sendEmailConfirmBid: function (req, res) {
+        var idItem = req.params.id;
+        var user = req.session.user;
+        var email = user.Email;
+        var price = req.body.price;
+        var name = req.body.name;
+
+        const nodemailer = require('nodemailer');
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'cainaykhongthat@gmail.com',
+                pass: 'matkhaucuamail'
+            }
+        });
+        var mailOptions = {
+            from: 'cainaykhongthat@gmail.com',
+            to: email,
+            subject: 'Confirm Bid Price',
+            text: 'You bid \"'+'{{name}}'+'\"\nWith price '+price+'\nClick link below to confirm:\n' +
+            'http://' + req.headers.host + '/item/'+idItem+'/bid?price='+price
+        };
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+    },
+    bid: function (req, res) {
+        var user = req.session.user;
+        if (user){
+            var idItem = req.params.id;
+            var price = req.query.price;
+            var idUser = user.IdUser;
+            var timebid=momment().format('YYYY/MM/DD H:mm:ss');
+            auctionitemdb.bid(idUser, idItem, price, timebid).then(function () {
+                return res.redirect('/item/'+idItem);
+            }).fail(function (err) {
+                console.log(err);
+                res.end('fail');
+            });
+        }
+        else{
+            res.end('fail');
+        }
+    }
+};
 module.exports = auctionitemController;
--- public/js/item.js
+++ public/js/item.js
@@ -1,3 +1,6 @@
+var bidPrice = NaN;
+var useSuggerPrice=false;
+
 function showSuggest(max, step) {
 
   var html = '<ul>';
@@ -9,7 +12,121 @@ function showSuggest(max, step) {
   document.getElementById('show-suggest-price').innerHTML = html;
 
 }
+
+$(document).ready(function() {
+    $("#bid-warning").hide()
+    $('#myModal').on('shown.bs.modal', function() {
+        $("#bid-warning").hide()
+    });
+    $('#custom-price').on('input', function(){
+        useSuggerPrice=false;
+    });
+});
+
 function bidWithPrice(price){
-  console.log(price);
-  
+    bidPrice = price;
+    useSuggerPrice=true;
 }
+
+$("#addwishlist").click(function () {
+    var idProduct = $(this).val();
+    var ajaxElement = {
+        checking : true
+    };
+    var ObjectItem = {
+        idItem : idProduct
+    };
+    $.ajax({
+        url: "/wishlist",
+        method: 'POST',
+        contentType : "application/json",
+        data: JSON.stringify({ajax : ajaxElement, idItem : ObjectItem}),
+        success: function (data) {
+            // xet redirect
+            if (typeof data.redirect == 'string') {
+                window.location.replace(window.location.protocol + "//" + window.location.host + data.redirect);
+            }
+            else {
+                // co nghia la da dang nhap va tra ve thanh cong
+                // thanh cong minh se lam gi ?
+                //1. minh se cho hien 1 cai popup len thong bao da thanh cong
+                //2. neu minh da them san pham nay roi thi minh se in ra la da co san pham nay roi
+                console.log(data.messagesFail);
+                console.log(data.messagesSuccess);
+                if( data.messagesFail !== undefined) {
+                    // that bai
+                    var bodymessage = $("#primary").find(".modal-body h3");
+                    bodymessage.css("color", "red");
+                    bodymessage.text(data.messagesFail);
+                    $("#popupMessages").click();
+                }
+                else {
+                    // thanh cong
+                    var bodymessage = $("#primary").find(".modal-body h3");
+                    bodymessage.css("color", "black");
+                    console.log(data.messagesSuccess);
+                    bodymessage.text(data.messagesSuccess);
+                    $("#popupMessages").click();
+                }
+            }
+        }
+    });
+});
+
+$("#btn-bid").click(function () {
+    var currentPrice = parseInt($("#current-price").text());
+    var customPrice = parseInt($("#custom-price").val());
+    var bidStep = parseInt($("#bid-step").text());
+    var bidWarning = $("#bid-warning");
+    if (!isNaN(customPrice) && !useSuggerPrice){
+        if(customPrice<currentPrice){
+            bidWarning.show();
+            bidWarning.text("Custom price must be large current price")
+            bidPrice = NaN;
+        }
+        else if(customPrice % bidStep !== 0){
+            bidWarning.show();
+            bidWarning.text("Custom price must be divisible by step")
+            bidPrice = NaN;
+        }
+        else{
+            bidWarning.hide();
+            bidPrice=customPrice;
+        }
+    }
+    console.log(bidPrice);
+    if(!isNaN(bidPrice)){
+        $('#myModal').modal('toggle');
+        BootstrapDialog.show(
+            {
+            title: 'Notify',
+            message: 'Your price is:  '+bidPrice+"\n We will send you a email to confirm",
+            buttons: [
+                {
+                    label: 'Close',
+                    action: function(dialog){
+                        dialog.close();
+                    }
+                },
+                {
+                label: 'Send me an email',
+                cssClass: 'btn-primary',
+                action: function(dialog) {
+                    var idItem= $("#addwishlist").val();
+                    var idName= $("#item-name").text();
+                    $.ajax({
+                        url: "/item/"+idItem+"/send_email_confirm_bid",
+                        method: 'POST',
+                        contentType : "application/json",
+                        data: JSON.stringify({price: bidPrice, name: idName}),
+                        success: function (data) {
+                            console.log(data);
+                        }
+                    });
+                    dialog.close();
+                }
+            }
+            ]
+        });
+    }
+});
